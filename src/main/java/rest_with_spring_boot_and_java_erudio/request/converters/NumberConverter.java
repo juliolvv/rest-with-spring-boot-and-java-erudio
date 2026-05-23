2e781b75966f6bfff741f85a72773d25320d339b
juliolvv <juliocesarolv13@gmail.com>
2026-05-23 16:40:29 -0300

    Refatorando código e estruturando classe SimpleMath

---
+++ src/main/java/rest_with_spring_boot_and_java_erudio/request/converters/NumberConverter.java
@@ -0,0 +1,19 @@
+package rest_with_spring_boot_and_java_erudio.request.converters;
+
+import rest_with_spring_boot_and_java_erudio.exception.UnsupportedMathOperationException;
+
+public class NumberConverter {
+
+    public static Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty())
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+        return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
+    }
+}
